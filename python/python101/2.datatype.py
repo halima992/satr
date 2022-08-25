894d44d3e3601d6e58294de797304cf37cc60fdb
Halima Josh <studentimamu@gmail.com>
2022-08-25 13:29:28 +0300

    add file explain diffrent types and how change from type to another

---
+++ python/python101/2.datatype.py
@@ -0,0 +1,22 @@
+# string
+
+my_string = "Hello world"
+print(my_string)
+print(type(my_string))
+#---------------------------------------
+
+# numbers
+
+age = 30
+weight = 50.1
+complex_num = 10j
+print("type age: ",type(age))
+
+#---------------------------------------
+
+# change types
+
+age = float(age)  # from integer to float
+print("type of age after the convereted",type(age))
+
+# others type str & int
